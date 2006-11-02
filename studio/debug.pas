6373ca51d359565a66a18e7973d9145812ba4212
John Newbigin <jnewbigin@chrysocome.net>
2006-11-03 10:52:32 +1100

    stdout & stderr support

--- studio/debug.pas
+++ studio/debug.pas
@@ -4,24 +4,43 @@ unit debug;
 {$MODE Delphi}
 {$ENDIF}
 
-// $Header: /home/cso/jnewbigin/cvsroot/rawwrite/studio/debug.pas,v 1.1 2004/12/30 13:50:08 jnewbigin Exp $
+// $Header: /home/cso/jnewbigin/cvsroot/rawwrite/studio/debug.pas,v 1.2 2005/12/05 08:26:02 jnewbigin Exp $
 
 interface
+
+uses windows;
 
 procedure Log(S : String);
 type DebugEvent = procedure (S : String) of object;
 procedure SetDebug(d : DebugEvent);
 procedure UseWriteln;
+procedure UseStdError;
+
 
 type TWriteLine = class
 public
    procedure WriteLine(S : String);
 end;
 
+type TStdError = class
+private
+   h : THandle;
+public
+   constructor Create;
+   procedure WriteLine(S : String);
+end;
+
+function IsDebuggerPresent : Boolean;
+
+
 implementation
+
+uses winioctl;
 
 var
    fOnDebug : DebugEvent;
+
+type TIsDebuggerPresent = function : BOOL; stdcall;
 
 procedure Log(S : String);
 begin
@@ -41,8 +60,21 @@ begin
 {$IFDEF FPC}
    Writeln(S + #13);
 {$ELSE}
-   Writeln(S);
+   Writeln(Output, S);
 {$ENDIF}
+end;
+
+constructor TStdError.Create;
+begin
+   h := GetStdHandle(STD_ERROR_HANDLE);
+end;
+
+procedure TStdError.WriteLine(S : String);
+var
+   Done : DWORD;
+begin
+   S := S + #13 + #10;
+   WriteFile2(h, PChar(S), Length(S), done, nil);
 end;
 
 procedure UseWriteln;
@@ -52,5 +84,53 @@ begin
    wl := TWriteLine.Create;
    SetDebug(wl.WriteLine);
 end;
+
+procedure UseStdError;
+var
+   se : TStdError;
+begin
+   se := TStdError.Create;
+   SetDebug(se.WriteLine);
+end;
+
+function IsDebuggerPresent : Boolean;
+var
+   hModule : hInst;
+//   Error : DWORD;
+   JIsDebuggerPresent : TIsDebuggerPresent;
+   P : Pointer;
+begin
+   Result := False;
+
+   // see if we can get IsDebuggerPresent from kernel32.dll
+
+   hModule := GetModuleHandle('kernel32.dll');
+   if hModule = 0 then
+   begin
+      // wininet is not yet loaded...
+      hModule := LoadLibrary('kernel32.dll');
+      {if hModule = 0 then
+      begin
+         Error := GetLastError;
+         //raise Exception.Create('Error loading Windows Internet Library.  ' + SysErrorMessage(Error));
+      end;}
+   end;
+
+   if hModule <> 0 then
+   begin
+      P := GetProcAddress(hModule, 'IsDebuggerPresent');
+      if P = nil then
+      begin
+         //raise Exception.Create('Could not find procedure ' + ProcName);
+      end
+      else
+      begin
+         JIsDebuggerPresent := p;
+         Result := JIsDebuggerPresent;
+      end;
+   end;
+
+end;
+
 
 end.
