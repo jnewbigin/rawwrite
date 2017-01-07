2253e55c688f57b2262373e75742cf49729520b1
John Newbigin <john.newbigin@rea-group.com>
2017-01-07 13:45:41 +1100

    Go direct to bash

--- .buildkite/upload-pipeline.bat
+++ .buildkite/upload-pipeline.bat
@@ -3,5 +3,5 @@ REM Do enough to get us into bash
 cd "%~dp0"
 dir
 echo running "C:\Program Files\Git\git-bash.exe" -c "./%~n0"
-"C:\Program Files\Git\git-bash.exe" -c "./%~n0"
+"C:\Program Files\Git\bin\bash" -c "./%~n0"
 echo done
