776b5ae32c3de83938ea1fd2fd728b0385cc3d64
John Newbigin <john.newbigin@rea-group.com>
2017-01-07 13:37:58 +1100

    More bootstrap

--- .buildkite/upload-pipeline.bat
+++ .buildkite/upload-pipeline.bat
@@ -7,4 +7,6 @@ echo %x0
 echo %~n0
 echo %dp0
 echo %~dp0
-"C:\Program Files\Git\git-bash.exe" --cd-to-home -c "cd %~n0 ; ./%~dp0"
+echo running "C:\Program Files\Git\git-bash.exe" --cd-to-home -x -c "cd %~dp0 ; ./%~n0"
+"C:\Program Files\Git\git-bash.exe" --cd-to-home -x -c "cd %~dp0 ; ./%~n0"
+echo done
