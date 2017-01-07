4c3949cbbbaf712a1174109511792a28b20b6e69
John Newbigin <john.newbigin@rea-group.com>
2017-01-07 13:43:15 +1100

    Almost there

--- .buildkite/upload-pipeline.bat
+++ .buildkite/upload-pipeline.bat
@@ -1,13 +1,7 @@
 @echo off
 REM Do enough to get us into bash
-echo %0
-echo %~0
-echo %n0
-echo %x0
-echo %~n0
-echo %dp0
-echo %~dp0
 cd "%~dp0"
-echo running "C:\Program Files\Git\git-bash.exe" -x "./%~n0"
-"C:\Program Files\Git\git-bash.exe" -x "./%~n0"
+dir
+echo running "C:\Program Files\Git\git-bash.exe" -c "./%~n0"
+"C:\Program Files\Git\git-bash.exe" -c "./%~n0"
 echo done
