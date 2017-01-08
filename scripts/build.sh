c78d3a0fa7e3dd98cf1963221ffd2f1d3ad6790d
John Newbigin <john.newbigin@rea-group.com>
2017-01-09 08:02:14 +1100

    Test build parameters

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,9 +5,16 @@ set -e -u -o pipefail
 
 . $(dirname $0)/../.buildkite/env.sh
 
-set -x
+echo $*
+exit 1
 
-#/c/lazarus/fpc/3.0.0/bin/i386-win32/ppcrossx64.exe  -Twin64 -Px86_64 -MDelphi -Scaghi -CirotR -O1 -gw2 -godwarfsets -gl -gh -Xg -gt -l -vewnhibq -Fistudio -Fistudio\\md5 -Fistudio\\random -Fustudio -Fustudio\\md5 -Fustudio\\random -FuC:\\lazarus\\lcl\\units\\x86_64-win64\\win32 -FuC:\\lazarus\\lcl\\units\\x86_64-win64 -FuC:\\lazarus\\components\\lazutils\\lib\\x86_64-win64 -FuC:\\lazarus\\packager\\units\\x86_64-win64 -Fu. -dLCL -dLCLwin32 -dBorland -dVer150 -dDelphi7 -dCompiler6_Up -dPUREPASCAL dd.lpr
+TARGET=Debug${BITS}
+LTARGET=$(echo $TARGET | tr [A-Z] [a-z])
+BITS=32
 
-/c/lazarus/lazbuild.exe --lazarusdir=c:\\lazarus dd.lpi
+/c/lazarus/lazbuild.exe --lazarusdir=c:\\lazarus dd.lpi --build-mode=$TARGET
 
+mv dd.exe dd${LTARGET}.exe
+put_artifact dd${LTARGET}.exe 
+
+next_step test${BITS}
