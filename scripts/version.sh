c56a528ee1b671365a6ecbc533ed94a728f4c5a0
John Newbigin <john.newbigin@rea-group.com>
2017-01-11 08:52:00 +1100

    Be less verbose

--- scripts/version.sh
+++ scripts/version.sh
@@ -3,14 +3,17 @@
 
 set -e -u -o pipefail
 
-set -x
-
 . $(dirname $0)/../.buildkite/env.sh
 
 APP_VERSION=$(grep 'const AppVersion' studio/studio_tools.pas | cut -d "'" -f 2)
 
+BITS="32 64"
+
+echo "Building dd version $APP_VERSION for $BITS"
+
 set_metadata version "$APP_VERSION"
 
-next_step build debug 32
-next_step build debug 64
+for BIT in $BITS ; do
+	next_step build debug $BIT
+done
 
