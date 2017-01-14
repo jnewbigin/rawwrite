a18f5b20399e01772a3bb5228955d7343fd924ba
John Newbigin <john.newbigin@rea-group.com>
2017-01-14 22:02:01 +1100

    typo

--- scripts/test.sh
+++ scripts/test.sh
@@ -15,7 +15,7 @@ BITS=$2
 FILE=dd${TARGET}${BITS}.exe
 get_artifact $FILE
 
-scripts/run_tests.sh $FILE
+scripts/run-tests.sh $FILE
 
 #next_step test $TARGET ${BITS}
 if [ "$TARGET" = "debug" ] ; then
